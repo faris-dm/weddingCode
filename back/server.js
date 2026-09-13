3301ac31ff990929e5590a246a51f6b0a52429eb
faris Naser <solonaser9@gmail.com>
2026-09-13 10:27:26 -0700

    feat:changed the cors weblink from localhost to the verel to commicate  with the backend and exchange data

--- back/server.js
+++ back/server.js
@@ -3,14 +3,15 @@ import bot from "./bot.js";
 import path from "./routes/back.js";
 import "dotenv/config";
 const app = express();
-import cors from "cors"
+import cors from "cors";
 
 app.use(express.json());
-app.use("/api",path)
-app.use(cors({
-  origin: "http://localhost:5173",
-}))
-
+app.use("/api", path);
+app.use(
+  cors({
+    origin: "https://frontqr.vercel.app",
+  })
+);
 
 app.get("/main", (req, res) => {
   return res.status(200).json("it is working");
@@ -18,8 +19,8 @@ app.get("/main", (req, res) => {
 
 const port = 2400;
 bot.launch().catch((err) => {
-  console.error("Bot failed to launch:", err.message)
-})
+  console.error("Bot failed to launch:", err.message);
+});
 console.log("Bot is running...");
 //  this SIGINT WILL STOP THE BOT WHEN THE  DEV CLCIK ctrl+c
 process.once("SIGINT", () => bot.stop("SIGINT"));
